3c24d8a9e0f93084b0550259bfb35ddbd7ddba24
dtpnst <dtpnst@users.noreply.github.com>
2023-11-02 04:35:03 +0000

    deploy: bf3cf5dfb67427f6cdab4cf5eeecfec7b3891242

---
+++ precache-manifest.56f585cf275a7fbf42e624084334439a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314d494cc0b994e12415583bbd13a066",
+    "url": "/assignment-1-dtpnst/index.html"
+  },
+  {
+    "revision": "9bcd30446589c63fba4e",
+    "url": "/assignment-1-dtpnst/static/css/main.02971024.chunk.css"
+  },
+  {
+    "revision": "0240fa9b632d835fe60e",
+    "url": "/assignment-1-dtpnst/static/js/2.e54176cb.chunk.js"
+  },
+  {
+    "revision": "9bcd30446589c63fba4e",
+    "url": "/assignment-1-dtpnst/static/js/main.fcc4bb7a.chunk.js"
+  },
+  {
+    "revision": "29347ffbf073c1ea619f",
+    "url": "/assignment-1-dtpnst/static/js/runtime~main.433a04ef.js"
+  }
+]);
